daa726ab33ff243a624f48a1d26c04721616fb63
UncleAlexKun <raisin27@mail.ru>
2021-03-12 23:26:06 +0400

    Начало

---
+++ GrabberAudio/GrabberAudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrabberAudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
